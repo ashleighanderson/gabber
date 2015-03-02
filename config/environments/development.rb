f4168b4743f92cdf13a17033f4b7554280d3923b
ashleighanderson <anderson.ashleighr@gmail.com>
2015-03-02 21:00:09 +0000

    devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,3 +35,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
+config.action_mailer.default_url_options = { host: 'chrome-tires-74-200861.use1-2.nitrousbox.com', port: 3000 }
+
+ 	
+
